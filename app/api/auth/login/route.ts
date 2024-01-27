a02fe977a551909a043747c18b7614d561920a75
gyorutan <waou1998@gmail.com>
2024-01-28 00:37:19 +0900

    login-api-fix

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -4,40 +4,46 @@ import User from "@/database/models/User";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-export const POST = async (req: NextRequest) => {
-  dbConnect();
-  const body = await req.json();
-  const { studentId, password } = body;
-  const user = await User.findOne({ studentId });
-  if (!user) {
-    return NextResponse.json({
-      success: false,
-      message: "아이디 또는 비밀번호가 일치하지 않습니다",
-    });
-  }
-  const comparePassword = await bcrypt.compare(password, user.password!);
-  if (!comparePassword) {
-    return {
-      success: false,
-      message: "아이디 또는 비밀번호가 일치하지 않습니다",
+dbConnect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json();
+    const { studentId, password } = body;
+    const user = await User.findOne({ studentId });
+    if (!user) {
+      return NextResponse.json({
+        success: false,
+        message: "아이디 또는 비밀번호가 일치하지 않습니다",
+      });
+    }
+    const comparePassword = await bcrypt.compare(password, user.password!);
+    if (!comparePassword) {
+      return {
+        success: false,
+        message: "아이디 또는 비밀번호가 일치하지 않습니다",
+      };
+    }
+
+    const payload = {
+      id: user.id,
     };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET!, {
+      expiresIn: "2h",
+    });
+
+    const response = NextResponse.json({
+      success: true,
+      message: "로그인되었습니다",
+      user,
+    });
+
+    response.cookies.set("token", token);
+
+    return response;
+  } catch (error: any) {
+    console.log(error);
+    return NextResponse.json({ success: false, error: error.message });
   }
-
-  const payload = {
-    id: user.id,
-  };
-
-  const token = jwt.sign(payload, process.env.JWT_SECRET!, {
-    expiresIn: "2h",
-  });
-
-  const response = NextResponse.json({
-    success: true,
-    message: "로그인되었습니다",
-    user,
-  });
-
-  response.cookies.set("token", token);
-
-  return response;
-};
+}
